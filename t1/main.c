946ec9f8bb25991565a556d4646a03a4edb15532
coelhod <dccdecastro@gmail.com>
2014-03-26 22:40:22 -0300

    script de teste pronto para heap e quick select

--- t1/main.c
+++ t1/main.c
@@ -2,24 +2,40 @@
 #include<stdlib.h>
 #include"instance.h"
 #include"common.h"
+#include"heap.h"
+#include"quick.h"
 
-int vet[35000000];
+#define NUM 1000 * 1 << 15
 
-int main ()
+int vet[NUM];
+int vet2[NUM];
+
+int main()
 {
-	int i;
+	int i, median;
+	float dt;
 
 	srand(time(NULL));
-
-	tic();
-	for(i = 1; i <= 15; i++)
-		instance_a(vet, 1000*(2<<(i-1)));
-	printf("Instance A: %.3fs\n", toc());
-
-	tic();
-	for(i = 1; i <= 15; i++)
-		instance_b(vet, 2<<(25-1), 2<<(i-1));
-	printf("Instance B: %.3fs\n", toc());
+	
+	for (i = 1; i <= 15; i++)
+	{
+		int n = 1000 * (1 << i);
+		printf("\nInput with %d elements.\n", n);
+		
+		instance_a(vet, n);
+		
+		copy_array(vet, vet2, n);
+		tic();
+		median = heap_select(vet2, n, n / 2);
+		dt = toc();
+		printf("Heap select found the median %d in %.3fs\n", median, dt);
+		
+		copy_array(vet, vet2, n);
+		tic();
+		median = quick_select(vet2, n, n / 2);
+		dt = toc();
+		printf("Quick select found the median %d in %.3fs\n", median, dt);
+	}
 
 	return 0;
 }
